89bb8c874fec8cc46b29f5f1da5963c080dbaf2d
Thiago Soares <tsoaresaz@gmail.com>
2022-11-26 13:06:00 -0300

    [feat] desenvolvendo Todo List com VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,212 @@
 <template>
-  <nav>
+  <div class="container">
+    <HeaderComponent @toSaveTask="save_task" />
+    <main class="main">
+      <div class="c-todo-list">
+        <ul class="c-todo-list__list">
+          <li class="c-todo-list__message">Tasks list is empty!</li>
+          <li class="c-todo-list__item" v-for="task of tasks" :key="task.id">
+            <input type="checkbox" class="c-todo-list__checkbox" />
+            <p class="c-todo-list__text">{{ task.name }}</p>
+            <button class="c-todo-list__remove" @click="delete_task(task)">
+              <img
+                src="https://nasratt.github.io/frontendMentorSolutions/projects/todoList/images/icon-cross.svg"
+                alt="Delete task button"
+              />
+            </button>
+          </li>
+        </ul>
+        <div class="c-todo-list-info">
+          <p class="c-todo-list-info__count">
+            <span class="c-todo-list-info__number">0</span>
+            items left
+          </p>
+          <button class="c-todo-list-info__button">Clear Completed</button>
+        </div>
+      </div>
+    </main>
+  </div>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
 </template>
 
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: "Test 01",
+        },
+        {
+          id: 2,
+          name: "Test 02",
+        },
+        {
+          id: 3,
+          name: "Test 03",
+        },
+        {
+          id: 4,
+          name: "Test 04",
+        },
+        {
+          id: 5,
+          name: "Test 05",
+        },
+      ],
+    };
+  },
+  methods: {
+    save_task(task) {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        name: task,
+      });
+    },
+    delete_task(task) {
+      console.log("clicked delete: ", task);
+      this.tasks = this.tasks.filter((item) => item.id !== task.id);
+      console.log("tasks update: ", this.tasks);
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./assets/scss/style.scss";
+
+.container {
+  margin: 0 auto;
+  max-width: toRem(540);
+  padding: toRem(48) toRem(25);
 }
 
-nav {
-  padding: 30px;
+.c-todo-list {
+  $self: &;
+  margin-top: toRem(20);
+  background-color: var(--color-25);
+  border-radius: toRem(8);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  &__item {
+    display: flex;
+    color: var(--color-ca);
+    font-size: toRem(18);
+    padding: toRem(15) toRem(20);
+    min-height: toRem(55);
+    background-color: var(--color-25);
+    border-bottom: 1px solid var(--color-39);
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &:hover {
+      #{ $self }__remove {
+        opacity: 1;
+      }
+    }
+  }
+
+  &__checkbox {
+    position: relative;
+    display: inline-block;
+    margin-right: toRem(30);
+    appearance: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: toRem(22);
+      height: toRem(22);
+      border-radius: 50%;
+      background: var(--color-39);
+      border: 1px solid var(--color-39);
+      position: absolute;
+      top: 0px;
+      left: -4px;
+      z-index: 5;
+      transition: all 0.2s ease-in-out;
+    }
+
+    &::after {
+      content: "";
+      display: inline-block;
+      width: toRem(18);
+      height: toRem(18);
+      border: none;
+      border-radius: 50%;
+      background: var(--color-25);
+      border: 1px solid var(--color-39);
+      position: absolute;
+      top: 2px;
+      left: -2px;
+      z-index: 10;
+      transition: all 0.2s ease-in-out;
+    }
+
+    &:hover {
+      &::before {
+        @include backgroundLinear;
+      }
+    }
+
+    &:checked {
+      &::after {
+        @include backgroundLinear;
+      }
+      + #{ $self }__text {
+        color: var(--color-39);
+        text-decoration: line-through;
+      }
+    }
+  }
+
+  &__text {
+    flex-grow: 1;
+  }
+
+  &__remove {
+    opacity: 0;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  &__message {
+    color: var(--color-ca);
+    padding: toRem(15) toRem(20);
+    text-align: center;
+  }
+}
+
+.c-todo-list-info {
+  display: flex;
+  justify-content: space-between;
+  padding: toRem(20);
+  color: var(--color-77);
+  font-size: toRem(16);
+  border-top: 1px solid var(--color-39);
+  transition: all 0.2s ease-in-out;
+
+  &__button {
+    color: var(--color-77);
+    font-size: toRem(16);
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: var(--color-ca);
     }
   }
 }
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,100 @@
+<template>
+  <header class="c-header">
+    <h1 class="c-header__title">Todo</h1>
+    <button class="c-header__button"></button>
+    <form class="c-header__form" @submit.prevent="add_task">
+      <input
+        type="text"
+        name=""
+        id=""
+        class="c-header__input"
+        placeholder="Create a new todo..."
+        v-model="task_model"
+      />
+      <button type="submit" class="c-header__submit">add</button>
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  emits: ["toSaveTask"],
+  data() {
+    return {
+      task_model: "",
+    };
+  },
+  methods: {
+    add_task() {
+      this.$emit("toSaveTask", this.task_model);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+
+.container {
+  margin: 0 auto;
+  max-width: toRem(540);
+  padding: toRem(48) toRem(25);
+}
+
+.c-header {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  &__title {
+    color: var(--color-f3);
+    font-size: $font-size-md;
+    font-weight: $font-weight-xbold;
+    letter-spacing: toRem(10);
+    text-transform: uppercase;
+  }
+
+  &__button {
+    width: toRem(30);
+    height: toRem(30);
+    background-color: transparent;
+    background-image: url("https://nasratt.github.io/frontendMentorSolutions/projects/todoList/images/icon-sun.svg");
+    background-repeat: no-repeat;
+    border: none;
+    cursor: pointer;
+  }
+
+  &__form {
+    display: flex;
+    width: 100%;
+    margin-top: toRem(40);
+    border-radius: toRem(8);
+    box-shadow: 0 5px 40px 10px rgba(var(--color-00-rgb), 0.25);
+  }
+
+  &__input {
+    border: none;
+    color: var(--color-ca);
+    height: toRem(50);
+    flex-grow: 1;
+    font-family: inherit;
+    font-size: toRem(18);
+    padding: toRem(20);
+    border-radius: toRem(8) 0 0 toRem(8);
+    background-color: var(--color-25);
+    caret-color: var(--color-ec);
+    outline: none;
+  }
+
+  &__submit {
+    border: none;
+    background-color: var(--color-16);
+    color: var(--color-ca);
+    padding: 0 toRem(10);
+    border-radius: 0 toRem(8) toRem(8) 0;
+    text-transform: uppercase;
+    cursor: pointer;
+  }
+}
+</style>
